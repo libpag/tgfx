257ec15be14c9f406ce288ca313d35ea78fb4c34
Fjie <ffjiefan@tencent.com>
2024-12-17 20:13:41 +0800

    整理代码

--- src/layers/record/ShapeLayerCmd.cpp
+++ src/layers/record/ShapeLayerCmd.cpp
@@ -17,160 +17,165 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "ShapeLayerCmd.h"
+#include "tgfx/layers/ShapeLayer.h"
 
 namespace tgfx {
 
 // TODO 实现下面的函数
 
 void CmdMakeShapeLayer::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    objMap[_id] = std::make_shared<ShapeLayer>();
+  objMap[_id] = ShapeLayer::Make();
 }
 
 nlohmann::json CmdMakeShapeLayer::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}};
 }
 
 void CmdSetPath::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setPath(_path);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setPath(_path);
+  }
 }
 
 nlohmann::json CmdSetPath::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"path", _path.toJson()}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"path", _path.toJson()}};
 }
 
 void CmdSetShape::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setShape(_shape);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setShape(_shape);
+  }
 }
 
 nlohmann::json CmdSetShape::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"shape", _shape->toJson()}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"shape", _shape->toJson()}};
 }
 
 void CmdSetFillStyle::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setFillStyle(_styleId);
-    }
+  auto it = objMap.find(_id);
+  auto styleIt = objMap.find(_styleId);
+  if (it != objMap.end() && styleIt != objMap.end()) {
+    auto style = std::static_pointer_cast<ShapeStyle>(styleIt->second);
+    std::static_pointer_cast<ShapeLayer>(it->second)->setFillStyle(style);
+  }
 }
 
 nlohmann::json CmdSetFillStyle::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"styleId", _styleId}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"styleId", _styleId}};
 }
 
 void CmdSetStrokeStyle::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeStyle(_styleId);
-    }
+  auto it = objMap.find(_id);
+  auto styleIt = objMap.find(_styleId);
+  if (it != objMap.end()) {
+    auto style = std::static_pointer_cast<ShapeStyle>(styleIt->second);
+    std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeStyle(style);
+  }
 }
 
 nlohmann::json CmdSetStrokeStyle::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"styleId", _styleId}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"styleId", _styleId}};
 }
 
 void CmdSetLineCap::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setLineCap(_cap);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setLineCap(_cap);
+  }
 }
 
 nlohmann::json CmdSetLineCap::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"cap", static_cast<int>(_cap)}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"cap", static_cast<int>(_cap)}};
 }
 
 void CmdSetLineJoin::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setLineJoin(_join);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setLineJoin(_join);
+  }
 }
 
 nlohmann::json CmdSetLineJoin::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"join", static_cast<int>(_join)}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"join", static_cast<int>(_join)}};
 }
 
 void CmdSetMiterLimit::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setMiterLimit(_limit);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setMiterLimit(_limit);
+  }
 }
 
 nlohmann::json CmdSetMiterLimit::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"limit", _limit}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"limit", _limit}};
 }
 
 void CmdSetLineWidth::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setLineWidth(_width);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setLineWidth(_width);
+  }
 }
 
 nlohmann::json CmdSetLineWidth::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"width", _width}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"width", _width}};
 }
 
 void CmdSetLineDashPattern::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setLineDashPattern(_pattern);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setLineDashPattern(_pattern);
+  }
 }
 
 nlohmann::json CmdSetLineDashPattern::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"pattern", _pattern}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"pattern", _pattern}};
 }
 
 void CmdSetLineDashPhase::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setLineDashPhase(_phase);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setLineDashPhase(_phase);
+  }
 }
 
 nlohmann::json CmdSetLineDashPhase::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"phase", _phase}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"phase", _phase}};
 }
 
 void CmdSetStrokeStart::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeStart(_start);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeStart(_start);
+  }
 }
 
 nlohmann::json CmdSetStrokeStart::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"start", _start}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"start", _start}};
 }
 
 void CmdSetStrokeEnd::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeEnd(_end);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeEnd(_end);
+  }
 }
 
 nlohmann::json CmdSetStrokeEnd::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"end", _end}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"end", _end}};
 }
 
 void CmdSetStrokeAlign::execute(std::map<int, std::shared_ptr<Recordable>>& objMap) {
-    auto it = objMap.find(_id);
-    if (it != objMap.end()) {
-        std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeAlign(_align);
-    }
+  auto it = objMap.find(_id);
+  if (it != objMap.end()) {
+    std::static_pointer_cast<ShapeLayer>(it->second)->setStrokeAlign(_align);
+  }
 }
 
 nlohmann::json CmdSetStrokeAlign::toJson() const {
-    return {{"type", static_cast<int>(getType())}, {"id", _id}, {"align", static_cast<int>(_align)}};
+  return {{"type", static_cast<int>(getType())}, {"id", _id}, {"align", static_cast<int>(_align)}};
 }
 
 }  // namespace tgfx
